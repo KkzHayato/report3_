58dc23e744f905ffff4a6f343a4f5104efe1f516
Hayato Kakazu <e225702@ie.u-ryukyu.ac.jp>
2022-11-28 19:37:32 +0900

    report4-2

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e225702/Warrior.java
@@ -0,0 +1,17 @@
+package jp.ac.uryukyu.ie.e225702;
+
+public class Warrior extends Hero{
+    
+    public Warrior(String name, int maximumHP, int attack){
+        super(name, maximumHP, attack);
+    }
+
+    public void attackWithWeponSkill(LivingThing opponent){
+        if (this.isDead() != true) {
+            int damage = this.getAttack() * 3/2;
+            System.out.printf("%sの攻撃！ウェポンスキルを発動！%sに%dのダメージを与えた！！", this.getName(), opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+}
